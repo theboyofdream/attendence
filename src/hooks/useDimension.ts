6c4a1b09e559ca084be7720ce96a05f77d096ef4
theboyofdream <infinityknight024@gmail.com>
2024-01-08 22:32:17 +0530

    feat: hook added for getting device width & height

---
+++ src/hooks/useDimension.ts
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Dimensions, ScaledSize } from 'react-native';
+
+export function useDimension() {
+  const [dimensions, setDimensions] = useState<ScaledSize>(Dimensions.get('screen'))
+
+  Dimensions.addEventListener('change', function (newDimensions) {
+    setDimensions(newDimensions.screen)
+  })
+
+  return { ...dimensions }
+}
