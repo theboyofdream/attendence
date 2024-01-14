b2c94ff0d569cc155c8bf7b575ece835473eee04
Abhishek <43162530+theboyofdream@users.noreply.github.com>
2024-01-14 20:20:21 +0530

    feat: added hook for responsive design

--- src/hooks/useDimension.ts
+++ src/hooks/useDimension.ts
@@ -1,16 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Dimensions, ScaledSize } from 'react-native';
 
 export function useDimension() {
   const [dimensions, setDimensions] = useState<ScaledSize>(Dimensions.get('screen'))
 
-  Dimensions.addEventListener('change', function (newDimensions) {
-    setDimensions(newDimensions.screen)
-  })
-
-  return { 
+  useEffect(() => {
+    Dimensions.addEventListener('change', function (newDimensions) {
+      setDimensions(newDimensions.screen)
+    })
+  }, [])
+  return {
     ...dimensions
-   }
+  }
   // return { 
   //   width: dimensions.width * dimensions.scale,
   //   height: dimensions.height * dimensions.scale
